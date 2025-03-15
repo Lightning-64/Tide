df80b2190ce3dc7b82831886143962fab49ce1bf
Lightning-64 <averyg8833@gmail.com>
2025-03-14 20:45:08 -0400

    Lectern support for fishing journal

--- fabric/src/main/java/com/li64/tide/datagen/providers/tags/TideItemTagsProvider.java
+++ fabric/src/main/java/com/li64/tide/datagen/providers/tags/TideItemTagsProvider.java
@@ -146,6 +146,8 @@ public class TideItemTagsProvider extends FabricTagProvider<Item> {
         getOrCreateTagBuilder(ItemTags.FISHES).forceAddTag(TideTags.Items.JOURNAL_FISH);
         getOrCreateTagBuilder(ItemTags.SWORDS).add(TideItems.BLAZING_SWORDFISH);
 
+        getOrCreateTagBuilder(ItemTags.LECTERN_BOOKS).add(TideItems.FISHING_JOURNAL);
+
         /* Compat tags */
 
         getOrCreateTagBuilder(TagKey.create(Registries.ITEM, new ResourceLocation("stardew_fishing", "starts_minigame")))
