9274237b18a7175e0eedda3ae845eac8b86809af
Lightning-64 <averyg8833@gmail.com>
2025-04-13 21:02:26 -0400

    Stardew fishing 2.0 compatibility fixes

--- forge/src/main/java/com/li64/tide/compat/stardewfishing/StardewFishingCompat.java
+++ forge/src/main/java/com/li64/tide/compat/stardewfishing/StardewFishingCompat.java
@@ -2,6 +2,7 @@ package com.li64.tide.compat.stardewfishing;
 
 import com.bonker.stardewfishing.StardewFishing;
 import com.bonker.stardewfishing.common.FishingHookLogic;
+import com.bonker.stardewfishing.common.init.SFSoundEvents;
 import com.li64.tide.registries.entities.misc.fishing.HookAccessor;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
@@ -15,11 +16,10 @@ public class StardewFishingCompat {
     public static boolean start(ServerPlayer player, HookAccessor hook, ItemStack rod, List<ItemStack> items) {
         if (items.stream().anyMatch(stack -> stack.is(StardewFishing.STARTS_MINIGAME))) {
             FishingHookLogic.getStoredRewards(hook).ifPresent(rewards -> rewards.addAll(items));
-            FishingHookLogic.startMinigame(player);
-            return true;
+            return FishingHookLogic.startStardewMinigame(player);
         } else {
-            FishingHookLogic.modifyRewards(items, 0);
-            player.level().playSound(null, player, StardewFishing.PULL_ITEM.get(), SoundSource.PLAYERS, 1.0F, 1.0F);
+            FishingHookLogic.modifyRewards(items, 0, rod);
+            player.level().playSound(null, player, SFSoundEvents.PULL_ITEM.get(), SoundSource.PLAYERS, 1.0F, 1.0F);
             return false;
         }
     }
