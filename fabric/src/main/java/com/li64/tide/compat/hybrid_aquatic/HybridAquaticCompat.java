b7961f39ad8ecd03abc5c5bf1808e8e85077ebda
Lightning-64 <averyg8833@gmail.com>
2025-10-16 19:13:52 -0400

    Hybrid aquatic 1.5 compatibility

--- fabric/src/main/java/com/li64/tide/compat/hybrid_aquatic/HybridAquaticCompat.java
+++ fabric/src/main/java/com/li64/tide/compat/hybrid_aquatic/HybridAquaticCompat.java
@@ -1,9 +1,6 @@
 package com.li64.tide.compat.hybrid_aquatic;
 
 import com.li64.tide.registries.entities.misc.fishing.TideFishingHook;
-import com.li64.tide.registries.items.TideFishingRodItem;
-import dev.hybridlabs.aquatic.enchantment.HybridAquaticEnchantments;
-import dev.hybridlabs.aquatic.enchantment.LiveCatchEnchantment;
 import dev.hybridlabs.aquatic.entity.HybridAquaticEntityTypes;
 import dev.hybridlabs.aquatic.item.HybridAquaticItems;
 import net.minecraft.core.BlockPos;
@@ -11,37 +8,25 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.animal.WaterAnimal;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.phys.Vec3;
-
-import java.util.HashMap;
 
 public class HybridAquaticCompat {
     public static Entity convertEntity(ItemEntity itemEntity, Player player, TideFishingHook hook) {
         Entity newEntity = null;
         ItemStack hookItem = hook.getHook();
 
-        if (hookItem.is(HybridAquaticItems.INSTANCE.getOMINOUS_HOOK())) {
-            var karkinosType = HybridAquaticEntityTypes.INSTANCE.getKARKINOS();
+        if (hookItem.is(HybridAquaticItems.INSTANCE.getOMINOUS_HOOK().get())) {
+            var karkinosType = HybridAquaticEntityTypes.INSTANCE.getKARKINOS().get();
             newEntity = createAndLaunchEntityAtPlayer(karkinosType, player, hook.blockPosition());
             if (newEntity != null) hook.clearHookItem();
         }
-        if (hookItem.is(HybridAquaticItems.INSTANCE.getCREEPERMAGNET_HOOK())) {
+        if (hookItem.is(HybridAquaticItems.INSTANCE.getCREEPERMAGNET_HOOK().get())) {
             var creeperType = EntityType.CREEPER;
             newEntity = createAndLaunchEntityAtPlayer(creeperType, player, hook.blockPosition());
             if (newEntity != null) hook.clearHookItem();
-        }
-        if (hook.getRod() != null && !hook.getRod().isEmpty()) {
-            if (EnchantmentHelper.getItemEnchantmentLevel(HybridAquaticEnchantments.INSTANCE.getLIVECATCH(), hook.getRod()) > 0) {
-                HashMap<Item, EntityType<? extends WaterAnimal>> ITEM_TO_ENTITY = LiveCatchEnchantment.Companion.getITEM_TO_ENTITYTYPE();
-                var entityType = ITEM_TO_ENTITY.get(itemEntity.getItem().getItem());
-                if (entityType != null) newEntity = createAndLaunchEntityAtPlayer(entityType, player, hook.blockPosition());
-            }
         }
 
         return newEntity == null ? itemEntity : newEntity;
